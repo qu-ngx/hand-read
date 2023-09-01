e1f2cbdf2b68b7ee6874cfdfb4cfdb21f14ce2df
qu-ngx <quang13012005@gmail.com>
2023-09-01 16:59:56 -0400

    Create dataset.py

--- dataset.py
+++ dataset.py
@@ -10,6 +10,7 @@ mp_hands = mp.solutions.hands
 mp_drawing = mp.solutions.drawing_utils
 mp_drawing_styles = mp.solutions.drawing_styles
 
+# Stats of a hand
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 DATA_DIR = './data'
@@ -18,21 +19,32 @@ DATA_DIR = './data'
 data = []
 labels = []
 
-
+# looping through all the files in directory and start inserting x and y to the dataset (data.pickle)
 for dir_ in os.listdir(DATA_DIR):
     for img_path in os.listdir(os.path.join(DATA_DIR, dir_)):
+
+        # Create a list to store auxilary data (Collecting inputs from the folders of images)
         data_aux = []
 
+        # Setting up x and y to hold the value of x, y when breaking down an image
         x_ = []
         y_ = []
 
+        # Reading each image from directory
+        # Read the images from path
         img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+        # Convert the color to RGB
         img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
+        # store the converted variable
         results = hands.process(img_rgb)
+
+        # Looping through all the hands
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 for i in range(len(hand_landmarks.landmark)):
+
+                    # Get x and y of hands
                     x = hand_landmarks.landmark[i].x
                     y = hand_landmarks.landmark[i].y
 
@@ -48,7 +60,7 @@ for dir_ in os.listdir(DATA_DIR):
             data.append(data_aux)
             labels.append(dir_)
 
-
+# Open data.pickle and dumb some columns
 f = open('data.pickle', 'wb')
-pickle.dump({'data': data, 'labels': labels}, f)
+pickle.dump({'data': data, 'labels': labels}, f)  # Dumping some columns
 f.close()
