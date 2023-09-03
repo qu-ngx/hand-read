6ee9b301fe8a90ce2131e6683c676d1db8156a1f
Quang Nguyen <91497379+qu-ngx@users.noreply.github.com>
2023-09-02 22:52:04 -0400

    Update inference.py

--- inference.py
+++ inference.py
@@ -18,7 +18,10 @@ mp_drawing_styles = mp.solutions.drawing_styles
 hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
 
 # Get labels for the machine learning models (The index represent the folder storing images for each label)
-labels_dict = {0: 'A', 1: 'B', 2: 'L'}
+labels_dict = {0: 'A', 1: 'B', 2: 'C', 3: 'D', 4: 'E', 5: 'F', 6: 'G',
+               7: 'H', 8: 'I', 9: 'K', 10: 'L', 11: 'M', 12: 'N', 13: 'O',
+               14: 'P', 15: 'Q', 16: 'R', 17: 'S', 18: 'T', 19: 'U', 20: 'V',
+               21: 'W', 22: 'X', 23: 'Y'}
 while True:
 
     data_aux = []
